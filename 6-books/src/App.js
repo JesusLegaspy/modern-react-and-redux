b61e935f78af243c77f5a0f2111466e23c19d988
Jesus Legaspy <jesus.legaspy@gmail.com>
2023-06-19 18:01:12 -0500

    Add, edit, delete books

--- 6-books/src/App.js
+++ 6-books/src/App.js
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+
 function App() {
-    return <div>
-        App
+  const [books, setBooks] = useState([]);
+
+  const editBookById = (id, newTitle) => {
+    setBooks(books.map(book => {
+      if (book.id === id) return ({ ...book, title: newTitle })
+      return book;
+    }));
+  };
+
+  const deleteBookById = (id) => {
+    setBooks(books.filter(book => book.id !== id));
+  };
+
+  const createBook = (title) => {
+    setBooks([...books, {
+      id: Math.round(Math.random() * 9999),
+      title
+    }]);
+  };
+
+  return (
+    <div className='app'>;
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+      <BookCreate onCreate={createBook} />
     </div>
+  );
 }
 
 export default App;
