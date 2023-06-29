ff1c42fa9d114798f5727cc49647bec04dc5f78e
Jesus Legaspy <jesus.legaspy@gmail.com>
2023-06-29 17:06:12 -0500

    Custom buttons

--- 7-comps/src/App.js
+++ 7-comps/src/App.js
@@ -1,5 +1,38 @@
+import { GoBellFill, GoCloud, GoCalendar, GoFlame } from 'react-icons/go';
+import Button from './Button';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button success rounded outline>
+          <GoBellFill />
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <GoFlame />
+          Buy now
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoCloud />
+          See deal
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          <GoCalendar />
+          Hide ads
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>Meow!</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
